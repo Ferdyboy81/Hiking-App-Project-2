b75ed4ca69da3cc7d8a008d8b11fd893f38e6edc
Kayli Nishime <kmnishime@ucdavis.edu>
2016-07-25 19:30:35 -0700

    reversed search bar

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,14 +2,14 @@ class WelcomeController < ApplicationController
 
   def index
     @trails = Trail.all
-
-  end
-
+end
 
 
   def show
     @trails = Trail.find(params[:id])
     @reviews = Review.where(trail_id: @trails).order("created_at DESC")
   end
+
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,7 @@ Rails.application.routes.draw do
   end
 
   get 'welcome/:id' => 'welcome#show', as: "trail_name"
-
-
-
+  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
